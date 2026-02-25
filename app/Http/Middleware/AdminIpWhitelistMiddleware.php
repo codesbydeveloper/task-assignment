aeb0a4f70d4f68c96a436c59064084d01e6b35e0
Abhinav Singh <abhinav.sense@gmail.com>
2026-02-25 18:36:48 +0530

    changes

--- app/Http/Middleware/AdminIpWhitelistMiddleware.php
+++ app/Http/Middleware/AdminIpWhitelistMiddleware.php
@@ -10,6 +10,7 @@ class AdminIpWhitelistMiddleware
 {
     public function handle(Request $request, Closure $next): Response
     {
+        dd($request->ip());
         if (app()->environment('local')) {
             return $next($request);
         }
